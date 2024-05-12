98a1e06d7acde91267554a87b1826441f7a7ea3e
Valetoriy <shadolproff420@mail.ru>
2024-05-13 01:26:16 +0300

    [lfp] l2 начало Slint

--- lfp/l2/src/main.rs
+++ lfp/l2/src/main.rs
@@ -1,13 +1,43 @@
+slint::include_modules!();
+
 #[derive(Debug, serde::Deserialize)]
 struct Profession {
+    index: usize,
     prof_name: String,
     traits: String,
 }
 
+// autocmd BufEnter *.slint :setlocal filetype=slint
+#[derive(Debug, serde::Deserialize)]
+struct Question {
+    index: usize,
+    question: String,
+}
+
 fn main() {
-    let mut rdr = csv::Reader::from_path("profs.csv").unwrap();
+    let mut rdr = csv::ReaderBuilder::new()
+        .delimiter(b';')
+        .from_path("db/profs.csv")
+        .unwrap();
     let profs: Vec<Profession> = rdr.deserialize().flatten().collect();
     for p in profs {
         println!("{p:?}");
     }
+
+    let mut rdr = csv::ReaderBuilder::new()
+        .delimiter(b';')
+        .from_path("db/questions.csv")
+        .unwrap();
+    let qs: Vec<Question> = rdr.deserialize().flatten().collect();
+    for q in qs {
+        println!("{q:?}");
+    }
+
+    let mw = MainWindow::new().unwrap();
+    // let mw_weak = mw.as_weak();
+    // mw.on_request_increase_value(move || {
+    //     let mw = mw_weak.unwrap();
+    //     mw.set_counter(mw.get_counter() + 1);
+    // });
+    mw.run().unwrap();
 }
