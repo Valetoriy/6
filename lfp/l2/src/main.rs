8b0200ab2747f61f2a8ed7b1fc8c2135e9e007c4
Valetoriy <shadolproff420@mail.ru>
2024-05-20 00:38:22 +0300

    [lfp] l2 главное меню

--- lfp/l2/src/main.rs
+++ lfp/l2/src/main.rs
@@ -33,11 +33,16 @@ fn main() {
         println!("{q:?}");
     }
 
-    let mw = MainWindow::new().unwrap();
-    // let mw_weak = mw.as_weak();
-    // mw.on_request_increase_value(move || {
-    //     let mw = mw_weak.unwrap();
-    //     mw.set_counter(mw.get_counter() + 1);
-    // });
-    mw.run().unwrap();
+    let mm = MainMenu::new().unwrap();
+    // let mm_weak = mm.as_weak();
+    mm.on_consult(move || {
+        println!("consult");
+    });
+    mm.on_profession_list(move || {
+        println!("p_list");
+    });
+    mm.on_question_list(move || {
+        println!("q_list");
+    });
+    mm.run().unwrap();
 }
